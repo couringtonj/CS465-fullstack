ab1d9b947b7f55de91081fb18709521009f6ba56
Jonathan Courington <jonathan.courington@outlook.com>
2022-06-25 21:05:03 -0400

    travlr website

---
+++ app_server/models/db.js
@@ -0,0 +1,50 @@
+const mongoose=require('mongoose');
+const host=process.env.DB_HOST||'127.0.0.1'
+const dbURI='mongodb://${host}/travlr';
+const readLine=require('readline');
+
+//avoid 'current server discovery and monitoring engine is depricated'
+mongoose.set('useUnifiedTopology',true);
+
+const connect=()=>{
+    setTimeout(()=>mongoose.connect(dbURI,{
+        useNewUrlParser:true,
+        useCreateIndex:true
+    }),1000);
+}
+
+mongoose.connection.on('connected',()=>{
+
+});
+
+mongoose.connection.on('error',err=>{
+
+});
+
+mongoose.connection.on('disconnected',()=>{
+
+});
+
+if(process.platform==='win32'){
+
+}
+
+const gracefulShutdown=(msg,callback)=>{
+
+};
+
+process.once('SIGUSR2',()=>{
+
+});
+
+process.on('SIGINT',()=>{
+
+});
+
+process.on('SIGTERM',()=>{
+
+});
+
+connect();
+//bring in the mongoose schema
+require('./travlr');
